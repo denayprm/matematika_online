335114dda2db050eb49761d14a53da368e293394
denayprm <152400121+denayprm@users.noreply.github.com>
2024-12-07 00:26:15 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -52,14 +52,14 @@ session_start();
                             <a class="nav-link" href="/matematika_online/profile">Profile</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/matematika_online/system/logout.php">Logout</a>
+                            <a class="nav-link" href="/matematika_online/auth/logout.php">Logout</a>
                         </li>
                     <?php else: ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/matematika_online/system/login.php">Login</a>
+                            <a class="nav-link" href="/matematika_online/auth/login.php">Login</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/matematika_online/system/register.php">Register</a>
+                            <a class="nav-link" href="/matematika_online/auth/register.php">Register</a>
                         </li>
                     <?php endif; ?>
                 </ul>
