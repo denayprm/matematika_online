82e256b4020706292e1f96409c1baaaa85431494
denayprm <152400121+denayprm@users.noreply.github.com>
2024-12-07 17:44:38 +0700

    Create sidebar.php

---
+++ includes/sidebar.php
@@ -0,0 +1,16 @@
+<div class="col-md-3 col-lg-2 d-none d-md-block bg-light sidebar">
+    <div class="position-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="<?= BASE_URL ?>admin/dashboard.php">
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= BASE_URL ?>admin/activity_logs.php">
+                    Aktivitas Logs
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
