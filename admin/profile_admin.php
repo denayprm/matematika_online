cfeded8dd2826d96424e09784a031e1729594483
denayprm <152400121+denayprm@users.noreply.github.com>
2024-12-08 02:05:27 +0700

    Update profile_admin.php

--- admin/profile_admin.php
+++ admin/profile_admin.php
@@ -1,10 +1,22 @@
+<?php
+require_once '../includes/auth_middleware.php';
+checkAdminAuth(); // Pastikan hanya siswa yang bisa akses
+
+require_once '../config/database.php';
+$db = new Database();
+$conn = $db->getConnection();
+
+include '../includes/header.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Profile Admin</title>
+    <!--<link rel="icon" type="image/x-icon" href="matematika_online/assets/img/denay.png">-->
 </head>
 
 <body>
@@ -12,3 +24,8 @@
 </body>
 
 </html>
+
+<?php
+include '../includes/footer.php';
+$db->closeConnection();
+?>
