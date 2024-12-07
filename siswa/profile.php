3b36d676ec6f48e04665484bd6179154e288c4da
denayprm <152400121+denayprm@users.noreply.github.com>
2024-12-07 17:45:13 +0700

    Update profile.php

--- siswa/profile.php
+++ siswa/profile.php
@@ -1,10 +1,22 @@
+<?php
+require_once '../includes/auth_middleware.php';
+checkSiswaAuth(); // Pastikan hanya siswa yang bisa akses
+
+require_once '../config/database.php';
+$db = new Database();
+$conn = $db->getConnection();
+
+include '../includes/header.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profile Siswa</title>
+    <!--<link rel="icon" type="image/x-icon" href="matematika_online/assets/img/denay.png">-->
 </head>
 
 <body>
@@ -12,3 +24,8 @@
 </body>
 
 </html>
+
+<?php
+include '../includes/footer.php';
+$db->closeConnection();
+?>
