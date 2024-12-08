95968ab65e31f3560cb0ba01eb904af1a7d23567
denayprm <152400121+denayprm@users.noreply.github.com>
2024-12-08 23:30:56 +0700

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,9 +4,4 @@ define('BASE_URL', 'http://localhost/matematika_online/');
 
 // Role definitions
 define('ROLE_ADMIN', 'admin');
-define('ROLE_GURU', 'guru');
 define('ROLE_SISWA', 'siswa');
-
-// Upload paths
-define('UPLOAD_PATH', ROOT_PATH . '/uploads');
-define('MAX_FILE_SIZE', 5242880); // 5MB
