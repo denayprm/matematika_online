0f0e4b3140598721de6606e52ba1baf314661047
denayprm <152400121+denayprm@users.noreply.github.com>
2024-12-08 23:31:08 +0700

    Create footer.php

---
+++ guest/footer.php
@@ -0,0 +1,9 @@
+</div>
+<footer class="bg-light text-center text-lg-start mt-4">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2024 Siti Maesaroh
+    </div>
+</footer>
+</body>
+
+</html>
