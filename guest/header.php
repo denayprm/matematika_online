176307004917f648bbeaecafa566daa21e7d0307
denayprm <152400121+denayprm@users.noreply.github.com>
2024-12-09 00:38:31 +0700

    Update header.php

--- guest/header.php
+++ guest/header.php
@@ -21,7 +21,7 @@ if (!defined('BASE_URL')) {
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link href="/matematika_online/assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
