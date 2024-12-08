8b2a8c9370fcfea6bb45b4f2a7b9131a7a9aa347
denayprm <152400121+denayprm@users.noreply.github.com>
2024-12-08 23:31:12 +0700

    Create header.php

---
+++ guest/header.php
@@ -0,0 +1,37 @@
+<?php
+
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+if (!defined('BASE_URL')) {
+    define('BASE_URL', 'http://localhost/matematika_online/');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Pembelajaran Matematika Online</title>
+    <link rel="icon" type="image/x-icon" href="matematika_online/assets/img/denay.png">
+
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="/matematika_online/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="<?= BASE_URL ?>">Matematika Online</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        </div>
+    </nav>
+    <div class="container mt-4"></div>
