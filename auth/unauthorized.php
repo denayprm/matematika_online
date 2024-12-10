2fbb317d019c637186af38ec98a22b732c32d035
Deni Permana <152400121+denayprm@users.noreply.github.com>
2024-12-10 18:50:34 +0700

    fix: unauthorized.php
    Signed-off-by: Deni Permana <152400121+denayprm@users.noreply.github.com>

--- auth/unauthorized.php
+++ auth/unauthorized.php
@@ -6,7 +6,7 @@ include 'includes/header.php';
     <div class="col-md-6 text-center">
         <h1>Akses Ditolak</h1>
         <p>Anda tidak memiliki izin untuk mengakses halaman ini.</p>
-        <a href="login.php" class="btn btn-primary">Kembali ke Login</a>
+        <a href="auth/login.php" class="btn btn-primary">Kembali ke Login</a>
     </div>
 </div>
 
