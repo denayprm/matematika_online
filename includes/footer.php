f31c129f97aaebd2871bc00689f2e0c2b3d071ee
Deni Permana <152400121+denayprm@users.noreply.github.com>
2024-12-10 17:58:48 +0700

    fix footer.php
    Signed-off-by: Deni Permana <152400121+denayprm@users.noreply.github.com>

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 </div>
 <footer class="bg-light text-center text-lg-start mt-4">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2024 Siti Maesaroh
+        © 2024 Deni Permana
     </div>
 </footer>
 </body>
